2088c297eae97c1b26760daeb72aeaabddd3a3ab
Gagan Deep Suri <gagan.deep.suri@gds.ey.com>
2025-07-31 01:11:25 +0530

    added dvc with params

--- src/model_building.py
+++ src/model_building.py
@@ -28,22 +28,22 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-# def load_params(params_path: str) -> dict:
-#     """Load parameters from a YAML file."""
-#     try:
-#         with open(params_path, 'r') as file:
-#             params = yaml.safe_load(file)
-#         logger.debug('Parameters retrieved from %s', params_path)
-#         return params
-#     except FileNotFoundError:
-#         logger.error('File not found: %s', params_path)
-#         raise
-#     except yaml.YAMLError as e:
-#         logger.error('YAML error: %s', e)
-#         raise
-#     except Exception as e:
-#         logger.error('Unexpected error: %s', e)
-#         raise
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 
 def load_data(file_path: str) -> pd.DataFrame:
@@ -67,8 +67,8 @@ def load_data(file_path: str) -> pd.DataFrame:
         logger.error('Unexpected error occurred while loading the data: %s', e)
         raise
 
-#def train_model(X_train: np.ndarray, y_train: np.ndarray, params: dict) -> RandomForestClassifier:
-def train_model(X_train: np.ndarray, y_train: np.ndarray) -> RandomForestClassifier:
+def train_model(X_train: np.ndarray, y_train: np.ndarray, params: dict) -> RandomForestClassifier:
+#def train_model(X_train: np.ndarray, y_train: np.ndarray) -> RandomForestClassifier:
     """
     Train the RandomForest model.
     
@@ -81,13 +81,13 @@ def train_model(X_train: np.ndarray, y_train: np.ndarray) -> RandomForestClassif
         if X_train.shape[0] != y_train.shape[0]:
             raise ValueError("The number of samples in X_train and y_train must be the same.")
         
-        #logger.debug('Initializing RandomForest model with parameters: %s', params)
-        logger.debug('Initializing RandomForest model with default parameters')
+        logger.debug('Initializing RandomForest model with parameters: %s', params)
+        #logger.debug('Initializing RandomForest model with default parameters')
         # Define default hyperparameters
-        n_estimators = 100
-        random_state = 42
-        #clf = RandomForestClassifier(n_estimators=params['n_estimators'], random_state=params['random_state'])
-        clf = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state)
+        # n_estimators = 100
+        # random_state = 42
+        clf = RandomForestClassifier(n_estimators=params['n_estimators'], random_state=params['random_state'])
+        #clf = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state)
         
         logger.debug('Model training started with %d samples', X_train.shape[0])
         clf.fit(X_train, y_train)
@@ -125,13 +125,13 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-        # params = load_params('params.yaml')['model_building']
+        params = load_params('params.yaml')['model_building']
         train_data = load_data('./data/processed/train_tfidf.csv')
         X_train = train_data.iloc[:, :-1].values
         y_train = train_data.iloc[:, -1].values
 
-        #clf = train_model(X_train, y_train, params)
-        clf = train_model(X_train, y_train)
+        clf = train_model(X_train, y_train, params)
+        #clf = train_model(X_train, y_train)
         
         model_save_path = 'models/model.pkl'
         save_model(clf, model_save_path)
